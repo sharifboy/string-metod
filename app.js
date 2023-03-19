b9d3fc6a4545fbf76b8bebda33d2cd7e344f77e0
sharifboy <sharifboy@gmail.com>
2023-03-19 11:52:27 +0500

    made initial layouts

---
+++ app.js
@@ -0,0 +1,39 @@
+/* ------ ko'p ishlatiladigan string metodlari -------*/
+
+let email = 'sharifboy@gmail.com'
+
+// lastindexOf()
+
+// let result = email.lastIndexOf('o')
+// console.log(result);
+
+// slice()
+
+// let result = email.slice(0, 9)
+// console.log(result);
+
+// substr()
+
+// let result = email.substr(1, 10).length
+// console.log(result);
+
+// replase()
+// let result = email.replace( 's', 'a')
+// console.log(result);
+
+// charAt() - birnchi harakterni olib beradi
+// let result = email.charAt()
+// console.log(result);
+
+// toUppercase - tolowercase
+// let result = email.toUpperCase()
+// console.log(result);
+
+// trim()
+// let result = email.trim()
+// console.log(result.length, email.length);
+
+// split()
+
+    let result = email.split('')
+    console.log(result);
